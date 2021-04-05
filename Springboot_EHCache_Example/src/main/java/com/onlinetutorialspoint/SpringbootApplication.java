4127eafe4fc707e8ed25da94419b23711082dc89
Pooja Monal Bhoyar <poojamonal.bhoyar@centurylink.com>
2021-04-05 15:09:07 +0530

    Inital Commit

---
+++ Springboot_EHCache_Example/src/main/java/com/onlinetutorialspoint/SpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.onlinetutorialspoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
